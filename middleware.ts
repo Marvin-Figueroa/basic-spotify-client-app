15f916e939e5ae62886a5cd726b941efd6db68aa
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-11 21:07:58 -0600

    Add middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = '/login';
+
+    if (requestedPage !== '/') {
+      url.search = `p=${requestedPage}`;
+    }
+
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/',
+};
