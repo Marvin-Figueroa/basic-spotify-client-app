2c1ed909b0116502044a501b2ac812179a456b93
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-19 23:28:15 -0600

    Modify secret in nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     signIn: '/login',
     signOut: '/',
