b5082a4776506f2833c2685682e62f2545eb4790
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-19 22:38:19 -0600

    Wrap the Card with a Link

--- components/Artist.tsx
+++ components/Artist.tsx
@@ -8,47 +8,47 @@ type ArtistProps = {
 
 function Artist({ artist }: ArtistProps) {
   return (
-    <Card css={{ w: '100%', h: '300px' }}>
-      <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-        <Col dir='colum'>
-          <Link
-            href={{
-              pathname: '/artist/[id]',
-              query: { id: artist.id },
-            }}>
+    <Link
+      href={{
+        pathname: '/artist/[id]',
+        query: { id: artist.id },
+      }}>
+      <Card css={{ w: '100%', h: '300px' }}>
+        <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+          <Col dir='colum'>
             <Badge color='error'>{artist.name}</Badge>
-          </Link>
-        </Col>
-      </Card.Header>
-      <Card.Body css={{ p: 0 }}>
-        <Card.Image
-          src={artist.images[1]?.url}
-          width='100%'
-          height='100%'
-          objectFit='cover'
-          alt='Artist Image'
-        />
-      </Card.Body>
-      <Card.Footer
-        isBlurred
-        css={{
-          position: 'absolute',
-          bgBlur: '#ffffff66',
-          borderTop: '$borderWeights$light solid rgba(255, 255, 255, 0.2)',
-          bottom: 0,
-          zIndex: 1,
-        }}>
-        <Row justify='flex-start'>
-          <Col>
-            <Row justify='center' align='center'>
-              <Badge size='md' variant='flat'>
-                ARTIST
-              </Badge>
-            </Row>
           </Col>
-        </Row>
-      </Card.Footer>
-    </Card>
+        </Card.Header>
+        <Card.Body css={{ p: 0 }}>
+          <Card.Image
+            src={artist.images[1]?.url}
+            width='100%'
+            height='100%'
+            objectFit='cover'
+            alt='Artist Image'
+          />
+        </Card.Body>
+        <Card.Footer
+          isBlurred
+          css={{
+            position: 'absolute',
+            bgBlur: '#ffffff66',
+            borderTop: '$borderWeights$light solid rgba(255, 255, 255, 0.2)',
+            bottom: 0,
+            zIndex: 1,
+          }}>
+          <Row justify='flex-start'>
+            <Col>
+              <Row justify='center' align='center'>
+                <Badge size='md' variant='flat'>
+                  ARTIST
+                </Badge>
+              </Row>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Link>
   );
 }
 
